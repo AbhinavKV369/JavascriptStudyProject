8f77673c29d7f9203a142f81dc60864a17f4ce70
AbhinavKV369 <abhinavkvmayyil@gmail.com>
2024-12-16 13:02:58 +0530

    Added Delete Button in JS

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
+// Id calling
 let kitchenInput = document.getElementById("kitchen-input")
 let addBtn = document.getElementById("add-btn")
 let kitchenItemList = document.getElementById("kitchen-item-list")
+let KitchenItems = [];
 
+// function declaration
 function addKitchenItemList(){
-    let kitchenInputData = kitchenInput.value
-    let li = document.createElement("li")
+    // Adding Elements
+    let kitchenInputData = kitchenInput.value;
+    let li = document.createElement("li");
     li.innerText = kitchenInputData;
     kitchenItemList.appendChild(li)
-    kitchenInput.value = "";
     kitchenInput.focus()
-}   
+    kitchenInput.value = ""
 
-addBtn.addEventListener("click",addKitchenItemList)
+    // Deleting Elements
+    let trashBtn = document.createElement("i");
+    trashBtn.classList.add('fas','fa-trash');
+    li.appendChild(trashBtn);
+}  
+function onEnter(event){
+    if(event.key === "Enter"){
+        addKitchenItemList()
+    }
+}
+function deleteKitchenItenList(event){
+    if(event.target.classList[0] === "fas"){
+       let item = event.target.parentElement;
+       item.remove()
+      
+    }
+}
+// Adding EventListeners
+addBtn.addEventListener("click",addKitchenItemList);
+kitchenInput.addEventListener("keypress",onEnter);
+kitchenItemList.addEventListener("click",deleteKitchenItenList)
